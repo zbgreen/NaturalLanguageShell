b0ab53051055d62541717b53851b5bc964a83ad9
Zach <zbgreen7@gmail.com>
2015-11-15 17:32:47 -0600

    Using 1 object for all commands. Fixed incorrectly handled errors/exceptions Added use cases at bottom of shell.py

--- shell.py
+++ shell.py
@@ -3,8 +3,8 @@ Zachary Green, Cindy La, Jonathan Price, Aaron Gershman
 
 Shell class to execute bash commands.
 """
-from subprocess import call, check_output
-
+import os
+from subprocess import call, check_output, CalledProcessError
 
 class Shell():
     """
@@ -12,40 +12,54 @@ class Shell():
     Executes the command using given inputs and prints out the bash command
     used.
     """
-    def __init__(self, id):
-        self.id = id
-        self.cmd = ""
+    def __init__(self):
+        self.cmd = []
+
+    # #Clear history.
+    # def c_history(self):
+    #     call("history", shell=True)
+
+    #See what user is running the commands
+    def whoami(self):
+        print(check_output(["whoami"]))
+        self.cmd.append("whoami")
 
     #Calls cat to print the given file
-    def print(self, file):
+    def cat(self, file):
         try:
-            self.cmd = "cat " + file
-            call(self.cmd)
-            print(self.cmd)
-        except NameError:
+            print(check_output(["cat", file]))
+            self.cmd.append("cat " + file)
+        except CalledProcessError:
             print("Invalid File name.")
 
-    #Writes str to end of given file. File must exist.
-    def write(self, str, file):
+    #Writes string to end of given file if it exists.
+    #If file doesn't exist it will create new file with given text.
+    def echo(self, file, str):
         try:
-            self.cmd = "echo " + str + " >> " + file
-            call(self.cmd, shell=True)
-        except FileNotFoundError:
-            print("File doesn't exist.")
+            s = "echo " + str + " >> " + file
+            check_output(s, shell=True)
+            self.cmd.append(s)
+        except TypeError:
+            print("Must pass string type.")
+
+    def cd_home(self):
+        os.chdir("/")
+        call("ls")
+        self.cmd.append("cd")
 
     #Go to the given directory. Incorrect directory doesn't cause errors so
     #check_output is used to check return code for success.
-    def goto(self, dir):
-        self.cmd = "cd " + dir
-        output = check_output(self.cmd, shell=True)
+    def cd(self, dir):
+        s = "cd " + dir
+        output = check_output(s, shell=True)
         if output == 0:
-            print(self.cmd)
+            print(self.output)
         else:
             print("Directory doesn't exist or can't be accessed.")
 
     #Open the given directory. Incorrect directory doesn't cause errors so
     #check_output is used to check return code for success.
-    def open_dir(self, dir):
+    def cd_multi(self, dir):
         self.cmd = "cd " + dir + "; ls"
         output = check_output(self.cmd, shell=True)
         if output == 0:
@@ -54,7 +68,7 @@ class Shell():
             print("Directory doesn't exist or can't be accessed.")
 
     #Copy 1 file to a given destination.
-    def copy_1(self, file, dest):
+    def cp(self, file, dest):
         self.cmd = "cp " + file + dest
         output = check_output(self.cmd, shell=True)
         if output == 0:
@@ -63,7 +77,7 @@ class Shell():
             print("Source file doesn't exist or can't be accessed.")
 
     #Copies multiple files to a given destination.
-    def copy_multiple(self, files, dest):
+    def cp_multi(self, files, dest):
         self.cmd = "cp "
         for file in len(files):
             self.cmd = self.cmd + " " + file
@@ -73,4 +87,26 @@ class Shell():
             print(self.cmd)
         else:
             print("1 or more source files don't exist or can't be accessed.")
+
+s = Shell()
+
+#done
+
+#whoami
+#s.whoami()
+
+#Print unexisting file
+#s.cat("t.txt")
+#Print existing file
+#s.cat("test.txt")
+
+#Try to add integer to end of file
+#s.echo("test.txt", 1)
+#Add string to end of file
+#s.echo("test.txt", "foo")
+#Create new file from given string
+#s.echo("t.txt", "foo")
+
+#In progress
+#s.cd_home()
 
